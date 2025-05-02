5c368569ff15fee5e9a10c78ad423569b2c2e0b4
TEABOX1 <1teabox1@gmail.com>
2025-05-02 20:51:58 +0300

    Init commit

---
+++ Assets/MainProject/Boot/Source/LoadingScreen.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Boot
+{
+    public class LoadingScreen : MonoBehaviour
+    {
+        [SerializeField] private Canvas _companyCanvas;
+        [SerializeField] private Canvas _gameNameCanvas;
+
+        //protected override void Awake()
+        //{
+        //    base.Awake();
+        //    _companyCanvas.enable = true;
+        //    _gameNameCanvas.enable = false;
+        //}
+    }
+};
